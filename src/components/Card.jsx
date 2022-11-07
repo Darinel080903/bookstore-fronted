68ac589e308cf70a6cae4324975a51385824ea8e
Darinel080903 <darinelmr12345@gmail.com>
2022-11-06 18:18:30 -0600

    images added in cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,17 +2,17 @@ import '../css/Card.css'
 import { Outlet, Link } from 'react-router-dom'
 
 
-function Card({image, titulo, texto}) {
+function Card(props) {
     return(
         <Link to="/Book" >
 
             <div className='card'>
-                <img className="imagen" src={image} />
-                <h4 className="titulo">{titulo}</h4>
-                <p className="texto">{texto}</p>
+                <img className="imagen" src={(`../src/assets/images/imagen-${props.imagen}.png`)} />
+                <h4 className="titulo">{props.titulo}</h4>
+                <p className="texto">{props.texto}</p>
                 <a href="" target="_blank">Comprar</a>
             </div>
-         </Link> 
+        </Link> 
     )
 }
 
