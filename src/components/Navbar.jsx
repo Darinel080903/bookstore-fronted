2ceebcac3e9f9be48a773c35459a4fc56b58f8d2
lyespinosa <leonardoyahelespinosa@gmail.com>
2022-11-17 18:15:48 -0600

    navbar fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,39 +13,53 @@ function Navbar(params) {
 
     const { user, setUser } = useContext(UserContext)
 
+    const { bookSearched, setBookSearched } = useState('')
+
+    function handleSearch() {
+        fetch('http://localhost:8080/book/name/' + bookSearched)
+            .then(response => response.json())
+            .then(data => setBooks(data.data));
+
+    }
+
     return (
         <nav className={styles.navbarDistributed}>
             <div className={styles.menu}>
                 <Menu />
             </div>
 
-            <div className={styles.searcher} id='searcher'>
-                <AiOutlineSearch className={styles.searchIcon}/>
-                <input className={styles.inputSearcher}     type="text" />
+            <div className={styles.searcherContainer}>
+                <div className={styles.searcher} id='searcher'>
+                    <AiOutlineSearch className={styles.searchIcon} />
+                    <form>
+                        <input className={styles.inputSearcher} type="text" />
+                    </form>
+                </div>
             </div>
 
             <div className={styles.logoContainer}>
                 <img className={styles.navbarLogoBook} src={Imagen1} />
             </div>
+
+            <div className={styles.optionsContainer}>
+                {user == null ?
+                    <div className={styles.buttonLogin}>
+                            <Link to="/login"><span>Log In</span></Link>
+
+
+                    </div>
+                    :
+
+                    <div className={styles.buttonLogin}>
+                        <div className={styles.userName}>{user.userName}</div>
+                        <button>
+                            <Link to="/compras">Compras</Link>
+                        </button>
+
+                    </div>
+                }
+            </div>
             
-            {user == null ?
-                <div className={styles.buttonLogin}>
-                    <button>
-                        <Link to="/login">Log In</Link>
-                        
-                    </button>
-
-                </div>
-                : 
-                
-                <div className={styles.buttonLogin}>
-                    <div className={styles.userName}>{ user.userName }</div>
-                    <button>
-                        <Link to="/compras">Compras</Link>
-                    </button>
-
-                </div>
-            }
 
         </nav>
     )
