02c7721f2b5652f89a0e1fb807a2020d0c87c81d
lyespinosa <leonardoyahelespinosa@gmail.com>
2022-11-21 11:37:30 -0600

    htmlFor fixed

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -6,8 +6,8 @@ function Menu() {
     return(
         <div className={styles.menuContainer}>
         <input type="checkbox" className={styles.menuToggle} id="menu-toggle"/>
-        <label className={styles.trigger} for="menu-toggle"></label>
-        <label className={styles.burger} for="menu-toggle"></label>
+        <label className={styles.trigger} htmlFor="menu-toggle"></label>
+        <label className={styles.burger} htmlFor="menu-toggle"></label>
         <ul   className={styles.menu}>
             <li className={styles.li}> <Link to="/">Home</Link> </li>
             <li className={styles.li}> <Link to="/bestseller">Best Sellers</Link> </li>
