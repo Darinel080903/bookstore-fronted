8600fc912d56031a36acf75e0b34431771686efa
JaviXDLOL16 <100722991+JaviXDLOL16@users.noreply.github.com>
2022-11-24 12:39:07 -0600

    A

--- src/components/VistaLibro.jsx
+++ src/components/VistaLibro.jsx
@@ -14,12 +14,42 @@ function VistaLibro() {
     const { nBook } = location.state;
 
 
+    const [ user, setUser ] = useState(localStorage.getItem("user-info"))
+
+    const [date, setDate] = useState(today.getDate()
+    );
+
     useEffect(() => {
-        fetch('http://localhost:8080/order' + nBook)
+        fetch('http://localhost:8080/book/' + nBook)
             .then(response => response.json())
             .then(data => setBooks(data.data));
 
     }, [])
+
+
+
+    const login = (e) => {
+        e.preventDefault();
+
+        fetch('http://localhost:8080/order', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                'email': email,
+                'password': password
+            })
+        })
+            .then(res => res.json())
+            .then(data => {
+                setUser(data.data)
+                setSuccess(data.success)
+            })
+            .catch(err => console.error(err))
+
+    }
+
 
     return (
         <div className={styles.contenedorCuadro}>
@@ -40,7 +70,7 @@ function VistaLibro() {
                             <p className={styles.price}>$ {books.price}</p>
                             <div className={styles.actions}>
                                 <button className={styles.buy}>Comprar</button>
-                                <button className={styles.addCart}><AiOutlineShoppingCart /></button>
+                                <button onClick={Alert} className={styles.addCart}><AiOutlineShoppingCart /></button>
 
                             </div>
 
