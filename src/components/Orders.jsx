1a57e0f3e7b57b91ffac231c9d611a0bed3d75bd
DiegoAlbertoPerezJuarez <105214062+DiegoAlbertoPerezJuarez@users.noreply.github.com>
2022-11-24 12:16:12 -0600

    addbook and orders

---
+++ src/components/Orders.jsx
@@ -0,0 +1,42 @@
+import { UserContext } from '../context/UserContext'
+import { useState, useRef, useContext, useEffect } from "react"
+import styles from '../css/Orders.module.css'
+
+function Orders() {
+
+    const [user, setUser] = useState(localStorage.getItem("user-info"));
+
+
+    const [orders, setOrders] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:8080/order/user/'+ user.id + '/AllOrders')
+            .then(response => response.json())
+            .then(data => setOrders(data.data));
+
+    }, [])
+
+    return (
+        <div className='container'>
+            <h2>My orders</h2>
+            <div className={styles.contenedorCuadro}>
+                {
+                    orders ?
+                    <div className={styles.cuadro}>
+                        <div className={styles.upPart}>
+                           <h2>{orders.status}</h2>
+                           <h2>{orders.quantity}</h2>
+                            <h2>{orders.price}</h2> 
+                        </div>
+                        
+                    </div>
+                    :
+                    <div className={styles.cuadro}>
+                    </div>
+                }
+            </div>
+        </div>
+      );
+}
+
+export default Orders;
