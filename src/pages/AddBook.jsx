48675f4543dbe004ec551ab25831708bb1b0f03b
lyespinosa <leonardoyahelespinosa@gmail.com>
2022-11-24 21:18:25 -0600

    advice added

--- src/pages/AddBook.jsx
+++ src/pages/AddBook.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import Navbar from "../components/NavbarForm";
 import Footer from "../components/Footer";
 import AddBookForm from "../components/AddBookForm";
+import Advice from '../components/Advice'
+
 
 function AddBook() {
     return ( 
         <>
         <Navbar/>
+        <Advice/>
         <AddBookForm/>
         <Footer/>
         </>
--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Advice from '../components/Advice'
+
 
 function ShoppingCart() {
   return (
     <div className=''>
         <Navbar/>
+        <Advice/>
         <Footer/>
     </div>
   )
