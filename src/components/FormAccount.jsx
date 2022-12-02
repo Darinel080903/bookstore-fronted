ea380bcaf4b13f21c2ce05e575b9908db7a7e4dd
Darinel Monjaras Ruiz <darinelmr12345@gmail.com>
2022-12-01 22:44:42 -0600

    changes in account

--- src/components/FormAccount.jsx
+++ src/components/FormAccount.jsx
@@ -1,22 +1,39 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useState, useEffect } from 'react'
+import Card from '../components/Card'
 
 function FormAccount() {
 
     const [user, setUser] = useState(localStorage.getItem("user-info"))
+    const[users, setUsers] = useState([]);
  
+    //FIXME:
     useEffect(() => {
         fetch('http://localhost:8080/user')
             .then(response => response.json())
             .then(data => setUser(data.data));
     }, [])
 
-
-    return(
     
-        <p>Nombre {user}</p>
-            
+    return(
+        <div>
+            {
+                users.map (user =>{
+                    return(
+                        <div key={user.id}>
+                            <Card
+                                id={user.id}
+                                lastName={user.lastName}
+                                birth={book.birth}
+                                phoneNumber={book.phoneNumber}
+                                email={book.email} 
+                            />
+                        </div>
+                    )
+                })
+            }
+        </div>
     )
 }
 
